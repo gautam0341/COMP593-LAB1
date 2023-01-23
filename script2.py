0553e9d564e04914a8a8a6c01a62611316af91b7
gautam0341 <Gautam.Singh@flemingcollege.ca>
2023-01-23 13:37:00 -0500

    Modified both files in the repo clone
    -Delete a print() statement from script1.py
    -Added two print() statement to script2.py

--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
 print("Although that way may not be obvious at first unless you're Dutch.")
 print("If the implementation is hard to explain, it's a bad idea.")
+print("Namespace are one honking idea -- let's do more of those!")
